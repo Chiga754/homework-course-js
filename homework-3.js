c43b235be04413bdec3717ddbc14edd37291a150
Chiga754 <a.chigarev96@gmail.com>
2021-11-16 18:20:56 +0500

    Выполнение домашнего задания на объекты.

---
+++ homework-3.js
@@ -0,0 +1,16 @@
+(function (){
+    // ! 1.Создать объект с полем product, равным ‘iphone’
+    const obj = {
+        product : 'iphone'
+    }
+
+    // ! 2.Добавить в объект поле price, равное 1000 и поле currency, равное ‘dollar’
+    obj.price = 1000;
+    obj.currency = 'dollar';
+    // ! 3.Добавить поле details, которое будет содержать объект с полями model и color
+    obj.details = {
+        model: '5s',
+        color: 'gold',
+    }
+    console.log(obj);
+})();
